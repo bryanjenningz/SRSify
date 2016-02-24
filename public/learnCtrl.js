1a94e76ce7eb247677a1e4647649579e7da6317d
Bryan Jennings <bryanjenningz@gmail.com>
2016-02-23 22:21:37 -0800

    add goal tracker

--- public/learnCtrl.js
+++ public/learnCtrl.js
@@ -1,12 +1,18 @@
 var learnApp = angular.module('learnApp', []);
 
 learnApp.controller('learnCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('cards.json').then(function(cards) {
-    $scope.cards = cards.data.sort(function(a, b) {
+  $http.get('cards.json').then(function(deck) {
+    $scope.deck = deck.data.sort(function(a, b) {
       return a.nextTime <= b.nextTime ? -1 : 1;
     });
+    $scope.total = Number(localStorage.getItem('total')) || 10;
+    localStorage.setItem('total', $scope.total)
+    $scope.cards = $scope.deck.slice(0, $scope.goal);
     $scope.index = 0;
     $scope.card = $scope.cards[$scope.index];
+    $scope.done = 0;
+    $scope.failed = 0;
+    $scope.unseen = 0;
   });
 
   var scores = {
