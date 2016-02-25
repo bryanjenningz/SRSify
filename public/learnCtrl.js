fce38d0d59247ed4871c15b1db864215f3365a7e
Bryan Jennings <bryanjenningz@gmail.com>
2016-02-24 23:52:47 -0800

    card stats change after being scored

--- public/learnCtrl.js
+++ public/learnCtrl.js
@@ -5,15 +5,16 @@ learnApp.controller('learnCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.deck = deck.data.sort(function(a, b) {
       return a.nextTime <= b.nextTime ? -1 : 1;
     });
-    $scope.total = Number(localStorage.getItem('total')) || 10;
-    localStorage.setItem('total', $scope.total)
+    $scope.total = Math.min(100, Number(localStorage.getItem('total')) || 10);
+    localStorage.setItem('total', $scope.total);
     $scope.cards = $scope.deck.slice(0, $scope.goal);
     $scope.index = 0;
     $scope.card = $scope.cards[$scope.index];
     $scope.done = 0;
     $scope.failed = 0;
-    $scope.unseen = 0;
+    $scope.unseen = $scope.total;
   });
+  $scope.seenCards = {};
 
   var scores = {
     'fail': 0,
@@ -22,6 +23,15 @@ learnApp.controller('learnCtrl', ['$scope', '$http', function($scope, $http) {
   };
 
   $scope.addScore = function(score) {
+    if (scores[score] === 0) {
+      $scope.failed++;
+    } else {
+      $scope.done++;
+    }
+    if (!$scope.seenCards[$scope.index]) {
+      $scope.seenCards[$scope.index] = true;
+      $scope.unseen--;
+    }
     $scope.index += 1;
     if ($scope.index >= $scope.cards.length) {
       $scope.index = 0;
